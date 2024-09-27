9db5e04219ea0f7f476339b1950e61044a743cbc
Oleg <oamalaxov@mail.ru>
2024-09-27 13:18:40 +0300

    Добавил try/exept

--- bot.py
+++ bot.py
@@ -1,57 +1,49 @@
 import telebot
 from config import TOKEN
-from orm import Links
-
 import os
 import yt_dlp
 
-bot = telebot.TeleBot(TOKEN)
-ls_links = {}
 
+
+bot = telebot.TeleBot(TOKEN)
+dict_links = {}
 
 @bot.message_handler(commands=['start'])
 def welcome(message) -> None:
     chat_id = message.chat.id
-    mess = (f'Добро пожаловать в бота! '
-            f'Введите ссылку')
+    mess = f'Введите ссылку на видео\n'
     bot.send_message(chat_id, mess)
-    ls_links[chat_id] = {}
+    dict_links[chat_id] = {}
     bot.register_next_step_handler(message, save_lin)
 
 def save_lin(message) -> None:
     chat_id = message.chat.id
     link = message.text
-    ls_links[chat_id] = link
+    dict_links[chat_id] = link
     bot.send_message(chat_id,
-                     f'Отлично, ваша ссылка добавлена, {link}')
-
-
-
-    l = Links(lin=link)
-    l.save()
-    bot.send_message(chat_id, f'Запись в БД прошла успешно!')
-    # for i in Links:
-    #     print(i.lin)
-
-
-    # url = input(f'Вставьте ссылку для скачивания: {link}')
+                     f'Отлично, ваша ссылка {link} '
+                     f'на видео-файл добавлена в очередь!'
+                     f'\nИдёт скачивание видео файла......')
     url = link
     print('Качаем!')
     # функция для скачивания видео из YouTube с помощью yt_dlp
     try:
         def download(url):
             ydl_opts = {
-                'format': 'mp4'
+                'merge_output_format': 'mp4',
+                'no-playlist': True,
+                'playlist_items': '1',
+                'format': 'bestvideo[height<=720]+bestaudio/best[height<=720]',
                 }
             with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download(link)
-
-        path = 'Videos form YouTube'
+                ydl.download(url)
+        path = 'Video'
         if path not in os.getcwd():
             os.chdir(path)
-
         download(url)
         print('Файл успешно скачан! Приятного просмотра!')
-
+        bot.send_message(chat_id,
+                         f'Загрузка видео-файла завершена, приятного просмотра!')
     except Exception:
-        print('Создайте папку <<Videos form YouTube>> для сохранения видео с Youtube и повторите попытку!')
+        print('Создайте папку <<Video>> для сохранения видео и повторите попытку!')
+        bot.send_message(chat_id, 'При попытке скачать видео возникла ошибка!')
