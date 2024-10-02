b5fd4df7a7291a3ec9a25f03f268cc60116570bc
Oleg <oamalaxov@mail.ru>
2024-10-02 12:26:07 +0300

    Переработал функции, изменил структуру

--- bot.py
+++ bot.py
@@ -7,35 +7,33 @@ from config import token
 
 bot = telebot.TeleBot(token)
 
+@bot.message_handler(commands=['start'])
+def handler(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, f'Привет! Это бот для загрузки видео-файлов.'
+                              '\nВведи ссылку на видео, которое хочешь загрузить.')
 
-# @bot.message_handler(commands=['start'])
-# def hendler(message):
-#     chat_id = message.chat.id
-#     bot.send_message(chat_id, f'Привет я бот!'
-#                               f'\nОтправь мне ссылку на видео-файл и я его скачаю для тебя')
 
-
-try:
-    # функция для скачивания файла
-    def download_video(url):
-        path = 'Video'
-        if path not in os.getcwd():
-            os.chdir(path)
-        ydl_opts = {
-            'merge_output_format': 'mp4',
-            'no-playlist': True,
-            'playlist_items': '1',
-            'format': 'bestvideo[height<=144]+bestaudio/best[height<=144]',
-            }
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([url])
-            print('Файл успешно скачан! Приятного просмотра!')
-except Exception:
-    print('Создайте папку <<Video>> для сохранения видео и повторите попытку!')
-    # @bot.message_handler()
-    # def errors(message):
-    #     chat_id = message.chat.id
-    #     bot.send_message(chat_id, f'Ошибка при скачивание видео-файла!')
+# функция для скачивания файла
+def download_video(url):
+    print('Идёт процесс скачивания.....')
+    path = 'Video'
+    if path not in os.getcwd():
+        os.chdir(path)
+    ydl_opts = {
+        'format': 'bestvideo[height<=240]+bestaudio/best[height<=240]',
+        'quiet': True,
+        'no_warnings': True,
+        'outtmpl': '%(id)s.%(ext)s',
+        'merge_output_format': 'mp4'
+        # 'merge_output_format': 'mp4',
+        # 'no-playlist': True,
+        # 'playlist_items': '1',
+        # 'format': 'bestvideo[height<=144]+bestaudio/best[height<=144]',
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+        print('Видео-файл успешно скачен!')
 
 
 def find_file():
@@ -48,15 +46,9 @@ def find_file():
 
 
 @bot.message_handler()
-def welcome(message) -> None:
-    # chat_id = message.chat.id
-    # bot.send_message(chat_id, f'Отлично, теперь твоя ссылка добавлена в очередь!')
-    # bot.send_message(chat_id, f'Идёт процесс скачивания видео-файла......')
+def handler(message) -> None:
+    chat_id = message.chat.id
     download_video(message.text)
     fpath = find_file()
     file = InputFile(fpath)
-    chat_id = message.chat.id
     bot.send_video(chat_id, file)
-    bot.send_message(chat_id,
-                     f'Загрузка видео-файла завершена, приятного просмотра!')
-
